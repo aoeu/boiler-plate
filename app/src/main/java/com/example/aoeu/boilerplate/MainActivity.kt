37c428b85adbd000734ec2606e4a11887bbf199d
Travis M <snp@s>
2018-07-22 17:09:40 -0400

    Add input form for name
    Add an input form for a name, a button to submit the name, which updates
    the greeting at the top right of the screen.
    
    Other incindental changes include:
    * Removing transparent status bar to avoid having to include status bar height in padding
    * Remove Windows line endings from XML and Kotlin source files
    * Fix tabstop identation differences (caused by editing files in Android Studio and vim and sam)

--- app/src/main/java/com/example/aoeu/boilerplate/MainActivity.kt
+++ app/src/main/java/com/example/aoeu/boilerplate/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.aoeu.boilerplate
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.greeting).setText(
+                resources.getString(R.string.greeting, resources.getString(R.string.defaultName))
+        )
+        findViewById<Button>(R.id.submitButton).setOnClickListener {
+            findViewById<TextView>(R.id.greeting).setText(
+                    resources.getString(R.string.greeting,
+                            findViewById<EditText>(R.id.nameInput).text ?: resources.getString(R.string.defaultName)
+                    )
+            )
+        }
     }
 }
